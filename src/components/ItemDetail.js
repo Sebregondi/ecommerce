eb5a8d33a607b5c116f926b73d5f3a24a5ead940
Esteban Landucci <emlanducci@gmail.com>
2022-10-09 13:01:53 -0300

    imagenes agregadas

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -8,29 +8,42 @@ const ItemDetail = () => {
     const [item, setItem] = useState({})
 
     useEffect(() => {
-        getItemDetails().then( response => {
+        getItemDetails().then(response => {
             setItem(response)
         })
     }, [])
-    
+
 
     const getItemDetails = () => {
         return new Promise((resolve, reject) => {
             setTimeout(() => {
-                resolve (products.find( p => p.id === Number (itemId) ) )
+                resolve(products.find(p => p.id === Number(itemId)))
             }, 1000);
         })
     }
 
-  return (
-    <div className='m-8'>
-        <div>{item.nombre}</div>
-        <div>{item.autor}</div>
-        <div>${item.precio}</div>
-        <div>{item.genero}</div>
-        <div>{item.pags}</div>
-    </div>
-  )
+    return (
+        <div className="card card-side bg-base-100 shadow-xl">
+            <figure><img src={item.img} alt={item.nombre} /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{item.nombre}</h2>
+                <strong>{item.precio}</strong>
+                <p>{item.autor}</p>
+                <p>{item.genero}</p>
+                <p>{item.pags}</p>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Comprar</button>
+                </div>
+            </div>
+        </div>
+        // <div className='m-8'>
+        //     <div>{item.nombre}</div>
+        //     <div>{item.autor}</div>
+        //     <div>${item.precio}</div>
+        //     <div>{item.genero}</div>
+        //     <div>{item.pags}</div>
+        // </div>
+    )
 }
 
 export default ItemDetail
