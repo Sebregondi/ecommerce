85dc93ab21834667bcf8d2207b88dd0dbfd1fb96
Esteban Landucci <emlanducci@gmail.com>
2022-10-09 13:20:33 -0300

    emprolijando rutas

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import PlatziContainer from './API/PlatziContainer';
 import ItemCount from './components/ItemCount';
 import ItemList from './components/ItemList';
 import ItemDetail from './components/ItemDetail';
+import ItemListContainer from './components/ItemListContainer';
+import ItemCard from './components/ItemCard';
 
 function PageNotFound() {
   return (
@@ -27,11 +29,13 @@ function App() {
     <BrowserRouter>
       <NavBar/>
       <Routes>
-        <Route path='/' element={<ItemCount />} />
-        <Route path='/list' element={<ItemList />} />
+        <Route path='/' element={<ItemListContainer />} />
+        <Route path='/item' element={<ItemCard />} />
         <Route path='/item/:id' element={<ItemDetail />} />
+        <Route path='/list' element={<ItemList />} />
+        <Route path='/count' element={<ItemCount />} />
         <Route path='*' element={<PageNotFound />} />
-        <Route path='/PZT' element={<PlatziContainer />} />
+        {/* <Route path='/PZT' element={<PlatziContainer />} /> */}
       </Routes>
     </BrowserRouter>
     </>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,11 +11,10 @@ const NavBar = () => {
             <div className="dropdown dropdown-left">
                 <label tabIndex={0} className="btn btn-sm m-1">Menu</label>
                 <ul tabIndex={0} className="dropdown-content menu p-2 outline shadow bg-base-100 rounded-box w-52">
-                    <li><Link to={'/PZT'}>Products</Link></li>
-                    <li><Link to={'/item'}>ItemList</Link></li>
-                    <li><Link to={'/'}>About us</Link></li>
-                    <li><Link to={'/'}>Shipping</Link></li>
-                    
+                    <li><Link to={'/'}>ItemListContainer</Link></li>
+                    <li><Link to={'/item'}>ItemCard</Link></li>
+                    <li><Link to={'/list'}>ItemList</Link></li>
+                    <li><Link to={'/count'}>ItemCount</Link></li>
                 </ul>
             </div>
             <CartWidget /> 
