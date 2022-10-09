f87cc37ee1538214c5dd7cab24043591a6b531dd
Esteban Landucci <emlanducci@gmail.com>
2022-10-09 19:04:33 -0300

    tamaño de cards en itemlistcontainer

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -23,7 +23,7 @@ const ItemListContainer = () => {
   return (
     <div>
         {items.map(i =>
-            <div key={i.id} className="mt-5 ml-5 outline outline-2 outline-red-400 inline-flex card card-compact w-96 bg-base-100 shadow-xl">
+            <div key={i.id} className="mt-5 ml-5 outline outline-2 outline-red-400 inline-flex card card-compact w-1/6 bg-base-100 shadow-xl">
             <figure><img src={i.img} alt={i.nombre} /></figure>
             <div className="card-body">
               <div className="card-actions justify-end">
