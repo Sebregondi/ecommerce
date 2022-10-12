fe1ad85cc24db869caafe557693388ad9f45c7fc
Esteban Landucci <emlanducci@gmail.com>
2022-10-12 10:22:58 -0300

    categorias en menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,10 +9,18 @@ const NavBar = () => {
                 <Link to='/' className="btn btn-ghost normal-case font-serif text-3xl">Dobby's Books</Link>
             </div>
             <div className="dropdown dropdown-left">
-                <label tabIndex={0} className="btn btn-sm m-1 bg-error">Menu</label>
+                <label tabIndex={0} className="btn btn-sm m-1 bg-error">Menú</label>
                 <ul tabIndex={0} className="dropdown-content menu p-2 outline outline-2 outline-red-400 shadow bg-base-100 rounded-box w-52">
-                    <li><Link to={'/'}>ItemListContainer</Link></li>
-                    <li><Link to={'/category/:id'}>ItemList</Link></li>
+                    <li><Link to={'/'}>Productos</Link></li>
+                    <li className="dropdown dropdown-left">
+                        <label tabIndex={0}><Link to={'/category/:id'}>Categorias</Link></label>
+                        <li tabIndex={0} className="dropdown-content dropdown-right menu p-2 outline outline-2 outline-red-400 shadow bg-base-100 rounded-box w-52"> 
+                            <li>Poesía</li>
+                            <li>Cuento</li>
+                            <li>En colaboración</li>
+                            <li>Entrevistas/Conferencias</li>
+                        </li> 
+                    </li>
                     <li><Link to={'/count'}>ItemCount</Link></li>
                 </ul>
             </div>
