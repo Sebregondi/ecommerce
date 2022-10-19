098f2f547d37e374e036066dd7efb855f7cb3e70
Esteban Landucci <emlanducci@gmail.com>
2022-10-19 18:36:04 -0300

    se inicializó firebase

--- src/index.js
+++ src/index.js
@@ -15,3 +15,18 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBAf91PvyS99juiWhnwqI2NUka2s_xxVJ4",
+  authDomain: "landucci38095.firebaseapp.com",
+  projectId: "landucci38095",
+  storageBucket: "landucci38095.appspot.com",
+  messagingSenderId: "491841014550",
+  appId: "1:491841014550:web:6d58c7119f693b0ac49e37"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
