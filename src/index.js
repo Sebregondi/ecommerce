0edb58fbecd7fc0bdcd26fbf65662613c8e9e4d1
Esteban Landucci <emlanducci@gmail.com>
2022-10-19 19:47:12 -0300

    se borraron comentarios

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,13 +12,10 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
 // Your web app's Firebase configuration
-import { initializeApp } from "firebase/app";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyBAf91PvyS99juiWhnwqI2NUka2s_xxVJ4",
