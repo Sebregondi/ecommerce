6e5a78e0ed5ee200ce7a1a53d7ec39829fabc24a
Esteban Landucci <emlanducci@gmail.com>
2022-10-20 19:23:07 -0300

    pruebas para ItemDetail

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,46 +1,71 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { products } from './products'
+import { collection, doc, getDocs, getFirestore } from "firebase/firestore"
+// import { useParams } from 'react-router-dom'
+// import { products } from './products'
 
 const ItemDetail = () => {
 
-    const { id: itemId } = useParams()
-    const [item, setItem] = useState({})
-
     useEffect(() => {
-        getItemDetails().then(response => {
-            setItem(response)
-        })
+      getItemDetails()
     }, [])
-
-
+    
     const getItemDetails = () => {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve(products.find(p => p.id === Number(itemId)))
-            }, 1000);
+        const db = getFirestore()
+        const itemsRef = collection (db, 'items')
+        getDocs(itemsRef).then( snapshot =>{
+            const data = snapshot.docs.find( i => ({id: i.id, ...i.data()}) )
+            console.log(data);
         })
     }
 
-    return (
-        <div className="card lg:card-side bg-base-100 shadow-xl">
-            <figure className="max-w-4xl px-5"><img src={item.img} alt={item.nombre}/></figure>
-            <div className="card-body">
-                <h2 className="card-title">${item.precio}</h2>
-                <div>
-                <p>Título: {item.nombre}</p>
-                <p>Autor: {item.autor}</p>
-                <p>Género: {item.categoryId}</p>
-                <p>Págs.: {item.pags}</p>
-                </div>
-                <p>Descripción: {item.sintesis}</p>
-                
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Agregar al carrito</button>
-                </div>
-            </div>
-        </div>
-    )
+  return (
+    <div>ItemDetail</div>
+  )
 }
 
 export default ItemDetail
+
+
+
+// const ItemDetail = () => {
+
+//     const { id: itemId } = useParams()
+//     const [item, setItem] = useState({})
+
+//     useEffect(() => {
+//         getItemDetails().then(response => {
+//             setItem(response)
+//         })
+//     }, [])
+
+
+//     const getItemDetails = () => {
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 resolve(products.find(p => p.id === Number(itemId)))
+//             }, 1000);
+//         })
+//     }
+
+//     return (
+//         <div className="card lg:card-side bg-base-100 shadow-xl">
+//             <figure className="max-w-4xl px-5"><img src={item.img} alt={item.nombre}/></figure>
+//             <div className="card-body">
+//                 <h2 className="card-title">${item.precio}</h2>
+//                 <div>
+//                 <p>Título: {item.nombre}</p>
+//                 <p>Autor: {item.autor}</p>
+//                 <p>Género: {item.categoryId}</p>
+//                 <p>Págs.: {item.pags}</p>
+//                 </div>
+//                 <p>Descripción: {item.sintesis}</p>
+                
+//                 <div className="card-actions justify-end">
+//                     <button className="btn btn-primary">Agregar al carrito</button>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default ItemDetail
--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import './index.css';
 import App from './App';
 import { initializeApp } from "firebase/app";
 
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_apiKey,
-//   authDomain: process.env.REACT_APP_authDomain,
-//   projectId: process.env.REACT_APP_projectId,
-//   storageBucket: process.env.REACT_APP_storageBucket,
-//   messagingSenderId: process.env.REACT_APP_messagingSenderId,
-//   appId: process.env.REACT_APP_appId
-// };
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
+};
 
-// const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 
 
@@ -23,18 +23,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-
-
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBAf91PvyS99juiWhnwqI2NUka2s_xxVJ4",
-  authDomain: "landucci38095.firebaseapp.com",
-  projectId: "landucci38095",
-  storageBucket: "landucci38095.appspot.com",
-  messagingSenderId: "491841014550",
-  appId: "1:491841014550:web:6d58c7119f693b0ac49e37"
-};
-
-
-const app = initializeApp(firebaseConfig);
