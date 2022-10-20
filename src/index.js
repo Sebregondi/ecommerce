7adc6cc047a48b217c2a9fef45d48a7d48793dc2
Esteban Landucci <emlanducci@gmail.com>
2022-10-20 11:35:54 -0300

    variables de entorno

--- src/index.js
+++ src/index.js
@@ -18,12 +18,12 @@ reportWebVitals();
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBAf91PvyS99juiWhnwqI2NUka2s_xxVJ4",
-  authDomain: "landucci38095.firebaseapp.com",
-  projectId: "landucci38095",
-  storageBucket: "landucci38095.appspot.com",
-  messagingSenderId: "491841014550",
-  appId: "1:491841014550:web:6d58c7119f693b0ac49e37"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
