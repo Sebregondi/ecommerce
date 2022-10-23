4edcd9ef3ad622e77cc02d548b8b3edd58e8b385
Esteban Landucci <emlanducci@gmail.com>
2022-10-23 14:19:22 -0300

    details funcionando

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const ItemCard = ({ id, title, img }) => {
     return (
         <div key={id} className="mt-5 ml-5 outline outline-2 outline-red-400 inline-flex card card-compact w-1/6 bg-base-100 shadow-xl">
-            <figure><img src={img} alt={title} /></figure>
+            <figure><img src={img} alt={title} height={200}/></figure>
             <div className="card-body">
                 <div className="card-actions justify-end">
                     <Link to={`/item/${id}`}>
--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,71 +1,49 @@
 import React, { useEffect, useState } from 'react'
-import { collection, doc, getDocs, getFirestore } from "firebase/firestore"
-// import { useParams } from 'react-router-dom'
-// import { products } from './products'
+import { collection, doc, getDoc, getFirestore } from "firebase/firestore"
+import { useParams } from 'react-router-dom'
 
 const ItemDetail = () => {
 
+  const { id } =useParams()
+
+  const [product, setProduct] = useState({})
+  const [loading, setLoading] = useState(true)
+
     useEffect(() => {
-      getItemDetails()
+      getItemDetail()
     }, [])
     
-    const getItemDetails = () => {
+    const getItemDetail = () => {
         const db = getFirestore()
-        const itemsRef = collection (db, 'items')
-        getDocs(itemsRef).then( snapshot =>{
-            const data = snapshot.docs.find( i => ({id: i.id, ...i.data()}) )
-            console.log(data);
-        })
+        const details = collection (db, 'items')
+        const docRef = doc (details, id)
+        getDoc(docRef).then( snapshot =>{
+          // console.log(snapshot.data());
+          setProduct(snapshot.data())
+          setLoading(false)
+        } )
     }
 
-  return (
-    <div>ItemDetail dsajkaskdjkjasd</div>
-  )
-}
-
-export default ItemDetail
-
-
-
-// const ItemDetail = () => {
-
-//     const { id: itemId } = useParams()
-//     const [item, setItem] = useState({})
-
-//     useEffect(() => {
-//         getItemDetails().then(response => {
-//             setItem(response)
-//         })
-//     }, [])
-
-
-//     const getItemDetails = () => {
-//         return new Promise((resolve, reject) => {
-//             setTimeout(() => {
-//                 resolve(products.find(p => p.id === Number(itemId)))
-//             }, 1000);
-//         })
-//     }
-
-//     return (
-//         <div className="card lg:card-side bg-base-100 shadow-xl">
-//             <figure className="max-w-4xl px-5"><img src={item.img} alt={item.nombre}/></figure>
-//             <div className="card-body">
-//                 <h2 className="card-title">${item.precio}</h2>
-//                 <div>
-//                 <p>Título: {item.nombre}</p>
-//                 <p>Autor: {item.autor}</p>
-//                 <p>Género: {item.categoryId}</p>
-//                 <p>Págs.: {item.pags}</p>
-//                 </div>
-//                 <p>Descripción: {item.sintesis}</p>
-                
-//                 <div className="card-actions justify-end">
-//                     <button className="btn btn-primary">Agregar al carrito</button>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default ItemDetail
+    return ( loading ? <h1 className='text-xl font-bold'>Cargando...</h1> 
+    :
+          <div className="card lg:card-side bg-base-100 shadow-xl">
+              <figure className="max-w-4xl px-5"><img src={product.img} alt={product.title}/></figure>
+              <div className="card-body">
+                  <h2 className="card-title">${product.price}</h2>
+                  <div>
+                  <p>Título: {product.title}</p>
+                  {/* <p>Autor: {item.autor}</p> */}
+                  <p>Género: {product.categoryId}</p>
+                  {/* <p>Págs.: {item.pags}</p> */}
+                  </div>
+                  <p>Descripción: {product.description}</p>
+                  
+                  <div className="card-actions justify-end">
+                      <button className="btn btn-primary">Agregar al carrito</button>
+                  </div>
+              </div>
+          </div>
+          )
+      }
+      
+      export default ItemDetail
