8c248e60f1baccecc213e6d88e2cfb6c79779466
Esteban Landucci <emlanducci@gmail.com>
2022-10-27 19:07:49 -0300

    pruebas con errores

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -1,38 +1,33 @@
-import { collection, getDocs, getFirestore, query, where } from 'firebase/firestore'
 import React, { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom'
+import { doc, getDoc, getFirestore } from 'firebase/firestore';
+import ItemDetail from './ItemDetail';
+import { useParams } from 'react-router-dom';
 
-const ItemDetailContainer = () => {
+export const ItemDetailContainer = () => {
 
     const { detailId } = useParams()
-
-    const [list, setList] = useState([])
-    const [category, setCategory] = useState([])
+    const [data, setData] = useState()
 
     useEffect(() => {
-        getItemList()
-        getCategory('Cuento')
+        const db = getFirestore()
+        const docRef = doc(db, 'items', '1LLjjMGoWuaTqMSCTA7c')
+        getDoc(docRef)
+            .then (res => setData({ id: res.id, ...res.data()}))
     }, [])
 
-    const getItemList = () => {
-        const db = getFirestore()
-        const collectionRef = collection(db, 'items')
-        getDocs(collectionRef).then(snapshot => {
-            const data = snapshot.docs.map(e => ({ id: e.id, ...e.data() }))
-            setList(data)
-        })
-    }
-
     return (
-        <>
-            <div>
-                <h1 className='m-3 text-3xl'>Nuestro catálogo</h1>
-            </div>
-            {list.map(l => <div className='m-3' key={l.id} {...l}>{l.title},
-                <Link to={`/category/:categoryId`}>{l.categoryId}</Link>
-            </div>)}
-        </>
+        <ItemDetail data={data} />
     )
 }
 
 export default ItemDetailContainer
+
+
+{/* <>
+        //     <div>
+                <h1 className='m-3 text-3xl'>Nuestro catálogo</h1>
+        //     </div>
+        //     {list.map(l => <div className='m-3' key={l.id} {...l}>{l.title},
+        //         <Link to={`/category/:categoryId`}>{l.categoryId}</Link>
+        //     </div>)}
+        // </> */}
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,38 +1,18 @@
-import React, { useEffect, useState } from 'react'
-import { collection, getDocs, getFirestore } from "firebase/firestore"
-import ItemCard from './ItemCard'
-import Loader from './Loader'
-import { useParams } from 'react-router-dom'
-
+import React from 'react'
 
 const ItemListContainer = () => {
 
-  const { categoryId } = useParams()
   
-  const [items, setItems] = useState([])
-  const [loading, setLoading] = useState(true)
+  return (
+    <div>ItemListContainer</div>
+  )
+}
 
-  useEffect(() => {
-    getItems()
-  }, [])
-  
-  const getItems = () => {
-    const db = getFirestore()
-    const collectionRef = collection(db, 'items')
-    getDocs (collectionRef).then (snapshot => {
-      const data = snapshot.docs.map ( e => ({id: e.id, ...e.data()}))
-      setItems(data)
-      setLoading (false)
-    })
-  }
+export default ItemListContainer
 
-return (
-    <div>
+
+{/* <div>
       { loading ? <Loader/> 
       :
       items.map(i => <ItemCard key={i.id} {...i} />)}
-    </div>)
-}
-
-
-export default ItemListContainer
+    </div> */}
